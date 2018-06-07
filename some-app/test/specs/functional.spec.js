6f5916c4ec5b5b0466d0006f52304ee651738016
joshgrant <joshua.m.grant@gmail.com>
2018-06-07 17:32:13 -0400

    adding some webdriverio tests

---
+++ some-app/test/specs/functional.spec.js
@@ -0,0 +1,15 @@
+const webdriver = require('webdriverio');
+
+describe('Temperature converter', () => {
+    it('should have the correct title', () => {
+
+    });
+
+    it('should show the correct conversion message', () => {
+
+    });
+
+    it('should have the logo', () => {
+
+    });
+});
