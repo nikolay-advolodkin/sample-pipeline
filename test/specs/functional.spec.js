8868284bea4f4a39802078c69e190e13014b7eda
joshgrant <joshua.m.grant@gmail.com>
2018-06-08 10:56:11 -0400

    starting with wdio tests

--- test/specs/functional.spec.js
+++ test/specs/functional.spec.js
@@ -1,15 +1,21 @@
 const webdriver = require('webdriverio');
+const assert = require('assert');
 
 describe('Temperature converter', () => {
     it('should have the correct title', () => {
+        browser.url('/');
+        
+        const actualTitle = browser.getText(".App-title");
+        const expectedTitle = "Welcome to Celcius to Farhenheit Calculator!"
 
+        assert.equal(actualTitle, expectedTitle);
     });
 
     it('should show the correct conversion message', () => {
-
+        assert.fail("not implemented");
     });
 
     it('should have the logo', () => {
-
+        assert.fail("not implemented");
     });
 });
